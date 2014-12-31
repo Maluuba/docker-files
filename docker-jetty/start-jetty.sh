fc2635f9d39fb81deec8d8f678d8186aeef79043
Usman Ismail <0xfffffff@gmail.com>
2014-12-31 13:41:00 -0500

    Use jetty user instead of root for jetty container. Also run jetty in forground now that we can use jetty exec to get inside a container

--- docker-jetty/start-jetty.sh
+++ docker-jetty/start-jetty.sh
@@ -32,8 +32,4 @@ then
 fi
 
 
-/opt/jetty/bin/jetty.sh restart
-
-#Override the exit command to prevent accidental container distruction 
-echo 'alias exit="echo Are you sure? this will kill the container. use Ctrl + p, Ctrl + q to detach or ctrl + d to exit"' > ~/.bashrc
-bash 
+/opt/jetty/bin/jetty.sh run
