901c647da6064f1a7328bb5ec3a5fabd7f9bd505
Justin Harris <justin.harris@maluuba.com>
2015-02-17 21:05:30 -0500

    nlp-toolset: corrections for mvn and rvm INFRA-306

--- docker-nlp-toolset/start-nlp-toolset.sh
+++ docker-nlp-toolset/start-nlp-toolset.sh
@@ -11,20 +11,28 @@ else
         echo "No init script found at $INIT_SCRIPT_PATH, you can see an example of the recommended script at https://github.com/Maluuba/deployment/blob/master$INIT_SCRIPT_PATH"
 fi
 
-curl nlptoolset.maluuba.com:81 > /dev/null
-
-# Go to NlpToolset folder
 NLP_TOOLSET_DIR=/NlpToolset
+
+cd $NLP_TOOLSET_DIR
+
 # TODO set password in $NLP_TOOLSET_DIR/config/environments/production.rb
+
+
+bundle install
+
+# Production mode
+RUN [RAILS_ENV=production] rake db:migrate
 
 mvn package
 cp $NLP_TOOLSET_DIR/target/NlpToolset.war /var/lib/tomcat7/webapps/nlptoolset/ROOT.war
 service tomcat7 restart
 
-sudo start nlptoolset
+start nlptoolset
 
 rvm use jruby@nlp-toolset
 
 rake jobs:work
 
 rails s
+
+curl localhost:3000 > /dev/null
