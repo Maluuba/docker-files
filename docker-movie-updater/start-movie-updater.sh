953357f50864aad072b90d6bc0284673d51c8106
pcota <petar.cota@maluuba.com>
2015-05-04 20:20:42 -0400

    Adding in the default files used by tomcat, with the extra ftp and gunzip needed

---
+++ docker-movie-updater/start-movie-updater.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+if [ -f /deployment/init.sh ];
+then
+	echo "Running custom init script"
+	chmod +x /deployment/init.sh
+	/deployment/init.sh
+fi
+
+if [ -d /deployment ];
+then
+	echo "Mapping deployed wars"
+	rm -rf /var/lib/tomcat7/webapps
+	ln -s /deployment /var/lib/tomcat7/webapps
+fi
+
+if [ -n "${Xmx}" ];
+then
+	sed -i s/Xmx.*\ /Xmx${Xmx}\ /g /etc/default/tomcat7
+fi
+
+if [ -n "${JAVA_OPTS}" ];
+then
+	# Add any Java opts that are set in the container
+	echo "Adding JAVA OPTS"
+	echo "JAVA_OPTS=\"\${JAVA_OPTS} ${JAVA_OPTS} \"" >> /etc/default/tomcat7
+fi
+
+chown tomcat7:tomcat7 /deployment
+service tomcat7 restart
+
+#Override the exit command to prevent accidental container distruction 
+echo 'alias exit="echo Are you sure? this will kill the container. use Ctrl + p, Ctrl + q to detach or ctrl + d to exit"' > ~/.bashrc
+
+#Run bash to keep container running and provide interactive mode
+bash
