a7e62f81e19e7676b790fc36fe3c88772a67224c
Yuan Li <yuan.li@maluuba.com>
2016-01-13 18:46:18 -0500

    movie-updater: change the cron config from 15 to 45

--- docker-movie-updater/start-movie-updater.sh
+++ docker-movie-updater/start-movie-updater.sh
@@ -35,7 +35,7 @@ aws s3 cp s3://${S3_PATH} /deployment/${LOCAL_PATH}
 aws s3 cp s3://maluuba-content/movie-update-scripts/updateMovies.sh /opt/updater/updateMovies.sh
 aws s3 cp s3://maluuba-content/movie-update-scripts/updateRatings.sh /opt/updater/updateRatings.sh
 chmod -R 777 /opt/updater
-(crontab -l 2>/dev/null; echo "*/15 * * * * /opt/updater/updateMovies.sh"; echo "0 9 * * * /opt/updater/updateRatings.sh"; echo "")| crontab -
+(crontab -l 2>/dev/null; echo "*/45 * * * * /opt/updater/updateMovies.sh"; echo "0 9 * * * /opt/updater/updateRatings.sh"; echo "")| crontab -
 
 if [ -n "${Xmx}" ];
 then
